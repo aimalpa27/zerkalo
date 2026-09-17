f9ba9885022ef46cf4f05cc0527ff9c3704bddc2
aimalpa27 <aimalpa.27@gmail.com>
2026-09-17 16:21:34 +0500

    fix: add generated schedule index rename migrations

---
+++ backend/apps/schedule/migrations/0002_rename_shifts_restaur_a1b2c3_idx_shifts_restaur_84d2a9_idx_and_more.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('schedule', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameIndex(
+            model_name='shift',
+            old_name='shifts_restaur_a1b2c3_idx',
+            new_name='shifts_restaur_84d2a9_idx',
+        ),
+        migrations.RenameIndex(
+            model_name='shift',
+            old_name='shifts_staff_d4e5f6_idx',
+            new_name='shifts_staff_i_e7e225_idx',
+        ),
+    ]
